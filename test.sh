c9ac67ba3edc81eb5c5d2707ffe7b2ef99ef71ed
Tom Mitchell <tmitchell@bbn.com>
2015-11-20 14:18:13 -0500

    Remove extraneous PATHs
    avail.sh and test.sh have a lot of extraneous PATH stuff. Remove it.

--- test.sh
+++ test.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
-export PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:$PATH
-export PATH=/usr/local/mysql/bin:$PATH
-export PATH=/usr/local/bin:$PATH
-# set PATH so it includes geni software if it exists
-# set PATH so it includes geni software if it exists
-if [ -d "/home/sedwards/gcf/src" ] ; then
-    PATH="/home/sedwards/gcf/src:/home/sedwards/gcf/examples:$PATH"
-    PATH="/home/sedwards/geni-lib/geni:$PATH"
-    export PYTHONPATH="/home/sedwards/gcf/src:$PYTHONPATH"
-    export PYTHONPATH="/home/sedwards/geni-lib:$PYTHONPATH"
-fi
-
-#echo $PATH
-#echo
-#echo $PYTHONPATH
-
 now=$(date +"%Y%m%d_%H%M%S")
-#echo $now
-
 file=/tmp/vmavail-$now.out
 echo Status Collected at $now > $file
-python /home/sedwards/geni-lib/samples/checkvmavail.py 
-#>> $file
+python /path/to/geni-expgraphs/checkvmavail.py
